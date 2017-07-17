c93b664224a6eec5accc2f8b3a3d47134e2f76c8
yukidox <yukidox@gmail.com>
2017-07-17 16:33:49 -0700

    Update reader.py

--- plutils/reader.py
+++ plutils/reader.py
@@ -2,24 +2,16 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jul 13 11:28:47 2017
-
 @author: yuqing
 """
 
-#import 
-import re
-
-def read_file(self):
-    with open(self, 'r') as i:
-        lines = str(i.read())
-    return lines
-
-
-class reader(str):
-    
-    def clean(file,string):
-        file = file.replace(string,'')
-        return file
+class reader:
+    def __init__(self,input_file):
+        with open(input_file, 'r') as i:
+            self.lines = str(i.read())
+        self.stage_keys = reader.read(self.lines,'\n','{')
+        self.stage_values = reader.read(self.lines,'{','}')
+        self.dict = reader.write_dict(self.stage_keys,self.stage_values)
     
     def read(resource, start_sep, end_sep):
         results = []
@@ -30,25 +22,19 @@ class reader(str):
                 results.append(par.split(end_sep)[0])
         return results  
     
+
     def write_dict(keys,values):
         dictionary = {}
         for i in range(len(keys)):
-            config_keys = reader.read(values[i],'\\n','=' ) 
-            config_values = reader.read(values[i],'=','\\n')
+            config_keys = reader.read(values[i],'\n','=' ) 
+            config_values = reader.read(values[i],'=','\n')
             dictionary[keys[i]] = {}
             for j in range(len(config_keys)):
-                config_keys[j].strip('\\')                       
-                config_values[j].strip('\\') 
-                
                 dictionary[keys[i]][config_keys[j]] = [config_values[j]]
         return dictionary
                 
-    def run(self):
-        stage_keys = reader.read(lines,'\\n','{')
-        stage_values = reader.read(lines,'{','}')
-        instruciton_dict = reader.write_dict(stage_keys, stage_values)
-        return instruciton_dict
-        
+
+
      
 
 
