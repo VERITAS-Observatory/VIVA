816ddbee2c48aa4a945bd928e4fa4d35e9cd4a42
yukidox <yukidox@gmail.com>
2017-07-19 15:42:49 -0700

    Update RunGroupManager.py
    return part dict when input groupstring seperated by :

--- plutils/RunGroupManager.py
+++ plutils/RunGroupManager.py
@@ -7,16 +7,30 @@ Created on Wed Jul 19 10:43:48 2017
 """
 
 class RunGroupManager:
-    def __init__(self, **kwargs):
-        self.gconfig = kwargs.get('GlobalConfig')
-        self.rldict = RunGroupManager.getitem(self)
+    def __init__(self, configdict, groupname):
+        self.configdict = configdict
+        self.gconfig = self.configdict.get('GlobalConfig')
+        self.groupdict = RunGroupManager.getdict(self)
+        self.groupname = groupname
+        self.partdict = RunGroupManager.getpart(self)
         
     
-    def getitem(self):
-        rldict = {}
+    def getdict(self):
+        groupdict = {}
+        i = 0
         for k,v in self.gconfig.items():
             if k.startswith('Runlist'):
-                rldict[k] = v
-        return rldict
+                groupdict['Group'+str(i)] = v
+                i += 1
+        return groupdict
+    
+    def getpart(self):
+        partdict = {}
+        for groupname in self.groupname.split(':'):
+            if groupname in self.groupdict.keys(): 
+                partdict[groupname] = self.groupdict[groupname]
+            else:
+                print ('no groupname ' + groupname)
+        return partdict
                 
 
